7c8ebee8927c1ff1c4823f112156a3cc4d362fa8
Dev-MukeshKumar <kpmukesh2001@gmail.com>
2022-10-21 23:42:26 +0530

    Created KMP search object and removed old test files

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,15 @@
+import scala.io.StdIn.readInt
+import scala.io.StdIn.readLine
+
 object Main extends App{
-  val n = scala.io.StdIn.readInt()
-  val k = scala.io.StdIn.readInt()
+  val n = readInt()
+  val inputArray = Array.ofDim[String](n * 2)
 
-  val x = SuperDigit.repeater(n,k)
+  for (i <- 0 until n * 2)
+    inputArray(i) = readLine()
 
-  println(SuperDigit.superDigit(x))
+  for (i <- 0 until n * 2 by 2) {
+    if (KmpSearch.search(inputArray(i), inputArray(i + 1))) println("Yes")
+    else println("No")
+  }
 }
